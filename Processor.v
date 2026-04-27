4a0f4f9da43f0f5d58b26fe909207f860af3d5bc
Noah Brown <brownnr9@miamioh.edu>
2026-04-26 22:13:19 -0400

    tested register to register instructions using benchmark

--- Processor.v
+++ Processor.v
@@ -504,7 +504,7 @@ module Processor
 				WRITE_BACK:
 					begin
 						//FOR IMM_IMM TEST
-							if(stored_instr[6:0] == REG_IMM && aluOut == 32'd18)
+							if(stored_instr[6:0] == REG_REG && aluOut == 32'd46)
 								LED<= 10'b0101001010;
 					
 						case( stored_instr [6:0] )
